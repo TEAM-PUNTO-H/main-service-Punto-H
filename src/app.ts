fcf0570e8bea01b53b8af8cf7f04bcded96b6354
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2025-12-01 11:14:01 -0500

    pequeño cambio

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,9 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000",
+ origin: (origin, callback) => {
+    callback(null, origin || "*");
+  },
     credentials: true,
 }));
 app.use(express.json());
